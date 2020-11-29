1bdb4209bd321fcf6d3d713678b6fac54584b8fb
rubastank <rubenmps@gmail.com>
2020-11-29 15:37:11 +0000

    Teste 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      ISTO É UM TESTE 70
-      
+      Pois claro que sim
+
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
